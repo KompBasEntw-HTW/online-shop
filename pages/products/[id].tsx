bc3e115468f8e1480d0f996ef8b36d9a55b3f081
Janosch Herrmann <janosch@janoschsworkspace.com>
2022-12-09 15:58:28 +0100

    update online shop to use new api endpoint

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -14,13 +14,13 @@ const ProductPage = () => {
   const { id } = router.query
 
   const productQueryFunction = async (id: string) => {
-    return fetch(`http://localhost/api/products/${id}`)
+    return fetch(`http://${process.env.NEXT_PUBLIC_HOSTNAME}/api/product-service/coffee/${id}`)
       .then(res => res.json())
       .catch(err => Promise.reject(err))
   }
 
   const relatedProductsQueryFunction = async () => {
-    return fetch('http://localhost/api/products')
+    return fetch(`http://${process.env.NEXT_PUBLIC_HOSTNAME}/api/product-service/coffee`)
       .then(res => res.json())
       .catch(err => Promise.reject(err))
   }
