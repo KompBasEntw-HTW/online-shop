f78bd3a558337044ddc00274da2934f569341fd8
Janosch Herrmann <janosch@janoschsworkspace.com>
2022-12-14 00:46:01 +0100

    update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,18 @@
-export type CoffeeProductData = {
+export type CoffeeSize = {
+  id: number
+  name: string
+  volumeDiscount: number
+  weightInGrams: number
+}
+
+export type Coffee = {
   id: number
   name: string
   description: string
   flavor: string
   imageUrl: string
-  price: number
-  availableBagSizes: {
-    id: number
-    name: string
-    priceModifier: number
-    weightInGrams: number
-  }[]
+  pricePerKilo: number
+  availableBagSizes: CoffeeSize[]
   flavorNotes: {
     id: number
     flavorNote: string
@@ -21,13 +23,79 @@ export type CoffeeProductData = {
   location: string
 }
 
-export type FilterOption = {
-  value: string
-  name: string
+export type SelectedRangeOptions = {
+  id: string
+  type: 'range'
+  min: number
+  max: number
 }
 
-export type ProductFilter = {
+export type SelectedCheckboxOptions = {
+  id: string
+  type: 'checkbox'
+  values: string[]
+}
+
+export type FilterOption = {
+  value: string
+  name?: string
+}
+
+export type CheckboxFilterType = {
   id: string
   name: string
-  options: FilterOption[]
+  type: 'checkbox'
+  values: FilterOption[]
 }
+
+export type RangeFilterType = {
+  id: string
+  name: string
+  type: 'range'
+  min: number
+  max: number
+  step: number
+}
+
+export type ProductFilter = CheckboxFilterType | RangeFilterType
+
+export type SortingsOptionsType = {
+  name: string
+  value: 'name-alpha' | 'name-reverse-alpha' | 'price-desc' | 'price-asc'
+}
+
+export type SelectedFilterOptions = SelectedRangeOptions | SelectedCheckboxOptions
+
+export type ShopPageState = {
+  mobileFiltersOpen: boolean
+  availableFilters: ProductFilter[]
+  queryState: {
+    searchQuery: string
+    filters: SelectedFilterOptions[]
+    sorting: SortingsOptionsType
+  }
+  filteredProducts: Coffee[]
+}
+
+export type ShopPageAction =
+  | { type: 'SET_MOBILE_FILTERS_OPEN'; payload: boolean }
+  | { type: 'SET_AVAILABLE_FILTERS'; payload: ProductFilter[] }
+  | { type: 'SET_FILTERED_PRODUCTS'; payload: Coffee[] }
+  | {
+      type: 'UPDATE_APPLIED_FILTERS'
+      payload: {
+        filterId: string
+        filterValue: string
+      }
+    }
+  | {
+      type: 'UPDATE_SEARCH_QUERY'
+      payload: string
+    }
+  | {
+      type: 'UPDATE_APPLIED_SORTING'
+      payload: SortingsOptionsType
+    }
+  | {
+      type: 'RESET_FILTERS'
+    }
