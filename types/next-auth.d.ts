90cd61432e682b3c1014d803d2bfad0412ca0124
Janosch Herrmann <janosch@janoschsworkspace.com>
2022-12-30 21:49:35 +0100

    update account page to reflect brand design and refactor empty state component to be more flexible

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,7 +2,7 @@ declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  interface Session {
+  type Session = {
     accessToken: string
     error: string
   }
