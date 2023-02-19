88f208942417c1ae402a0b180734c848473e1d93
Janosch Herrmann <janosch@janoschsworkspace.com>
2023-02-19 20:59:59 +0100

    add whitelist property to products hook, to get data for specific products

--- hooks/useProductsData.tsx
+++ hooks/useProductsData.tsx
@@ -4,7 +4,7 @@ import { Coffee } from '../types'
 const fetchProductsData = async (): Promise<Coffee[]> =>
   fetch(`/api/product-service/coffee`).then(res => res.json())
 
-const useProductsData = (limit?: number, blackList?: number[]) => {
+const useProductsData = (limit?: number, blackList?: number[], whiteList?: number[]) => {
   const { data, isLoading, isError, isSuccess } = useQuery(['products'], fetchProductsData)
 
   let products = data
@@ -12,6 +12,10 @@ const useProductsData = (limit?: number, blackList?: number[]) => {
 
   if (blackList) {
     products = products?.filter(product => !blackList.includes(product.id))
+  }
+
+  if (whiteList) {
+    products = products?.filter(product => whiteList.includes(product.id))
   }
 
   return { products, isLoading, isError, isSuccess }
